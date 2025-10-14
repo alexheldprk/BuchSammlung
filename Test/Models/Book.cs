52b0f4db206e6782450ea0268695a5ee61423427
Alexander Held <ah@wiko.de>
2025-10-14 11:59:57 +0200

    Buch Grundgerüst erstellt

---
+++ Test/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Der Titel wird benötigt!")]
+        public string Titel { get; set; }
+        [Required(ErrorMessage ="Der Autor wird benötigt!")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage ="Eine ISBN wird benötigt!")]
+        public string ISBN { get; set; }
+        [Required(ErrorMessage ="Das Erscheinungsjahr wird benötigt!")]
+        public int Jahr { get; set; }
+    }
+}
