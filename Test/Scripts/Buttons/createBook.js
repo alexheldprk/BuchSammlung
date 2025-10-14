038882ff011c57a1a7026c1ed6f8f8f074184a49
Alexander Held <ah@wiko.de>
2025-10-14 15:46:29 +0200

    Erstellen formular in html erstellt und javascript funktionen geschrieben

---
+++ Test/Scripts/Buttons/createBook.js
@@ -0,0 +1,31 @@
+﻿/*Seite Laden*/
+document.addEventListener("DOMContentLoaded", function () {
+
+    /*Referenziere Elemente aus HTML Formular*/
+    const popup = document.getElementById("popupFormular");
+    const oeffnenButton = document.getElementById("oeffnePopup");
+    const schliessenButton = document.querySelector(".schliessen");
+    const abbrechenButton = document.getElementById("abbrechen");
+    const formular = document.getElementById("buchform");
+
+    /*popup aufrufen/anzeigen*/
+    function popupOeffnen() {
+        popup.style.display = "block";
+    }
+
+ /*   popup schliessen/ausblenden*/
+    function popupSchliessen() {
+        popup.style.display = "none";
+    }
+
+    if (oeffnenButton) {
+        oeffnenButton.addEventListener("click", popupOeffnen);
+    }
+
+    if (schliessenButton) {
+        schliessenButton.addEventListener("click", popupSchliessen);
+    }
+
+
+
+})
