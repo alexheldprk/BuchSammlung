033eef57dd43af6cccca9489d71adee4881f53c8
Alexander Held <ah@wiko.local>
2025-10-15 17:19:00 +0200

    editformular und script korrigiert

--- Test/Scripts/Buttons/editBook.js
+++ Test/Scripts/Buttons/editBook.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     /*Referenziere Elemente aus HTML Formular*/
-    const popup = document.getElementById("popupFormular");
-    const oeffnenButton = document.querySelectorAll(".editButton");
+    const popup = document.getElementById("editPopup");
+    const oeffnenButton = document.querySelector(".editButton");
     const schliessenButton = document.querySelector(".schliessen");
     const abbrechenButton = document.getElementById("abbrechen");
     const formular = document.getElementById("buchform");
